cabad56fdd1a0abf0d503e3fc43285640f4d9d95
Austin Jesse Agaba <austinjesse256@gmail.com>
2025-02-17 11:02:07 +0300

    Update BNG.py

--- BNG.py
+++ BNG.py
@@ -1,5 +1,5 @@
 print("welcome to a band name generator")
 city = input("What's the name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
-print("Your band name could be " + city + " " +"pet")
+print("Your band name could be " + city + " " + pet)
 # Output: welcome to a band name generator
