881aab1f622672bd7a908b9fee6a3c2da3b9f0e6
Austin Jesse Agaba <austinjesse256@gmail.com>
2025-02-18 22:17:05 +0300

    Update if-elif-else.py

--- if-elif-else.py
+++ if-elif-else.py
@@ -1,14 +1,25 @@
 print("welcome to the rollercoaster")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 if height >= 120:
     print("You can ride the rollercoaster!")
     age = int(input("what is your age?"))
     if age < 12:
-        print("please pay $5.")
+        bill = 5
+        print("Child ticket $5.")
     elif age <= 18:
-        print("pay $7.")
+        bill = 7
+        print("Youth ticket $7.")
     else:
-        print("pay $12.")
+        bill = 12
+        print("Adult ticket $12.")
+        
+    wants_photo = input("Do you want a photo taken? Y for yes or N for no. ").strip().upper()
+    if wants_photo == "Y":
+        # Add $3 to the bill
+       bill += 3 # bill += 3
+        
+    print(f"Your final bill is ${bill}.")
 else:
     print("Sorry, you have to grow taller before you can ride.")
